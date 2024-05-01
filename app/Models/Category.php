503a598cd1e5f35e06a97da5d400d3336187d447
hungnm304 <hungnguyen.cfos@gmail.com>
2024-05-01 22:42:27 +0700

    Create categories table

---
+++ app/Models/Category.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name', 'slug',
+    ];
+
+    /**
+     * Validation rules for the model.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            '*' => [
+                'name' => 'required|max:100',
+                'slug' => 'required|max:100',
+            ],
+        ];
+    }
+}
